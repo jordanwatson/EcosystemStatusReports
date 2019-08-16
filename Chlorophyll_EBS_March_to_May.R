f60f39aeec43e0db60a0de768e1b3a3906b0fb92
Jordan Watson <jordanwatson@users.noreply.github.com>
2019-08-16 12:32:43 -0800

    Added VIIRS

--- Chlorophyll_EBS_March_to_May.R
+++ Chlorophyll_EBS_March_to_May.R
@@ -8,17 +8,15 @@ library(RCurl)
 library(forcats)
 
 
-statlkp <- readRDS("ERDDAP_Chlorophyll/Data/Stat_Lookup.RDS") %>% 
-  mutate(STAT_AREA=as.character(STAT_AREA),
-         lon=round(lon,2),
-         lat=round(lat,2))
-
 mydate <- data.frame(mydate=seq(as.Date("2006-03-01"), as.Date("2019-07-31"), "days")) %>% 
   mutate(year=format(mydate,"%Y"),
          month=format(mydate,"%m")) %>% 
   filter(month%in%c("03","04","05","06","07"))
 
-statlkp <- statlkp %>% 
+statlkp <- readRDS("ERDDAP_Chlorophyll/Data/Stat_Lookup.RDS") %>% 
+  mutate(STAT_AREA=as.character(STAT_AREA),
+         lon=round(lon,2),
+         lat=round(lat,2)) %>% 
   #inner_join(readRDS("../../PSESV_Manuscript/psesv_resubmission/Data/spatial_data.RDS") %>% 
   inner_join(readRDS("ERDDAP_Chlorophyll/Data/spatial_data.RDS") %>% 
   filter(m.depth<=-50 & m.depth>=-100) %>% 
@@ -60,7 +58,7 @@ for(i in 1:nrow(mydate)){
     rm(tempneg)
   }
 
-saveRDS(newdat,file="Data/EBS_Chlorophyll_Spring.RDS")
+saveRDS(newdat,file="ERDDAP_Chlorophyll/Data/EBS_Chlorophyll_Spring.RDS")
 
 
 
@@ -95,13 +93,16 @@ for(i in 1:nrow(mydate)){
   rm(tempneg)
 }
 
-saveRDS(nbsdat,file="Data/NBS_Chlorophyll_Spring.RDS")
+saveRDS(nbsdat,file="ERDDAP_Chlorophyll/Data/NBS_Chlorophyll_Spring.RDS")
 
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
 #  Now summarize the data
 #----------------------------------------------------------------------
+
+newdat <- readRDS("ERDDAP_Chlorophyll/Data/EBS_Chlorophyll_Spring.RDS")
+nbsdat <- readRDS("ERDDAP_Chlorophyll/Data/NBS_Chlorophyll_Spring.RDS")
 
 #Eastern Bering Sea
 test <- newdat %>% 
@@ -116,7 +117,7 @@ testsum <- test %>%
             chl=mean(chl,na.rm=TRUE)) %>% 
   ungroup %>% 
   mutate(Year=as.numeric(year),
-         Month=ifelse(month=="03","March",ifelse(month=="04","April","May")))
+         Month=month.name[as.numeric(month)])
 
 write_csv(testsum,"EBS_Summary_Chlorophyll.csv")
 
@@ -135,9 +136,10 @@ testsumnbs <- testnbs %>%
             chl=mean(chl,na.rm=TRUE)) %>% 
   ungroup %>% 
   mutate(Year=as.numeric(year),
-         Month=ifelse(month=="03","March",ifelse(month=="04","April","May")))
+         Month=month.name[as.numeric(month)])
+         #Month=ifelse(month=="03","March",ifelse(month=="04","April","May")))
 
-write_csv(testsum,"NBS_Summary_Chlorophyll.csv")
+write_csv(testsumnbs,"NBS_Summary_Chlorophyll.csv")
 
 
 
@@ -185,5 +187,181 @@ testsumnbs %>%
   scale_color_manual(values=c(OceansBlue1,SeagrassGreen1,WavesTeal3))
 dev.off()
 
+
+#--------------------------------------------------------------------------------
+# Now try with VIIRS data
+#--------------------------------------------------------------------------------
+
+mydate <- data.frame(mydate=seq(as.Date("2015-03-01"), as.Date("2019-07-31"), "days")) %>% 
+  mutate(year=format(mydate,"%Y"),
+         month=format(mydate,"%m")) %>% 
+  filter(month%in%c("03","04","05","06","07"))
+
+statlkpVIIRS <- readRDS("ERDDAP_Chlorophyll/Data/Stat_Lookup.RDS") %>% 
+  mutate(STAT_AREA=as.character(STAT_AREA),
+         lon=round(lon,2),
+         lat=round(lat,2)) %>% 
+  #inner_join(readRDS("../../PSESV_Manuscript/psesv_resubmission/Data/spatial_data.RDS") %>% 
+  inner_join(readRDS("ERDDAP_Chlorophyll/Data/spatial_data.RDS") %>% 
+               filter(m.depth<=-50 & m.depth>=-100) %>% 
+               dplyr::select(STAT_AREA,m.depth))
+
+
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+
+newdat <- data.frame()
+
+for(i in 1:nrow(mydate)){
+  print(mydate$mydate[i])
+  #URL <- "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.nc?analysed_sst[(2010-01-04T09:00:00Z):1:(2010-01-04T09:00:00Z)][(47):(68)][(-179.99):(-130.01)]"
+  x <- getBinaryURL(paste0("https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdVHNchla8day.nc?chla[(",mydate$mydate[i],"T09:00:00Z):1:(",mydate$mydate[i],"T09:00:00Z)][(0.0):1:(0.0)][(60):1:(56)][(-170):1:(-160)]"))
+  #https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdVHNchla8day.nc?chla[(2015-03-01T00:00:00Z):1:(2015-03-01T00:00:00Z)][(0.0):1:(0.0)][(60):1:(56)][(-170):1:(-160)]
+  
+  tmpSST <- tempfile(pattern="xwB", fileext=".nc")
+  writeBin(object=x, con=tmpSST)
+  nc <- nc_open(tmpSST)
+  
+  tempneg <- expand.grid(lon=ncvar_get(nc, varid = "longitude"),
+                         lat=ncvar_get(nc, varid = "latitude")) %>% 
+                         {bind_cols(.,data.frame(chl=as.vector(ncvar_get(nc,"chla"))))} #%>% 
+  #mutate(sst=ifelse(sst<(-2),-2,sst)) %>% 
+  #filter(!is.na(sst))
+  nc_close(nc)
+  
+  tempneg <- tempneg %>% 
+    mutate(lon=round(lon,2),
+           lat=round(lat,2)) %>% 
+    inner_join(statlkpVIIRS) %>% 
+    mutate(date=mydate$mydate[i])
+  
+  newdat <- newdat %>% 
+    bind_rows(tempneg)
+  
+  rm(tempneg)
+}
+
+saveRDS(newdat,file="ERDDAP_Chlorophyll/Data/EBS_Chlorophyll_Spring_VIIRS.RDS")
+
+
+#---------------------------------------------------------------------------------------------------
+#  Now do the same for the NBS with VIIRS data
+#---------------------------------------------------------------------------------------------------
+
+nbsdat <- data.frame()
+
+for(i in 1:nrow(mydate)){
+  print(mydate$mydate[i])
+  #URL <- "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.nc?analysed_sst[(2010-01-04T09:00:00Z):1:(2010-01-04T09:00:00Z)][(47):(68)][(-179.99):(-130.01)]"
+  x <- getBinaryURL(paste0("https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdVHNchla8day.nc?chla[(",mydate$mydate[i],"T09:00:00Z):1:(",mydate$mydate[i],"T09:00:00Z)][(0.0):1:(0.0)][(64):1:(60)][(-180):1:(-165)]"))
+
+  #https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdVHNchla8day.nc?chla[(2015-03-01T00:00:00Z):1:(2015-03-01T00:00:00Z)][(0.0):1:(0.0)][(60):1:(56)][(-170):1:(-160)]
+  
+  tmpSST <- tempfile(pattern="xwB", fileext=".nc")
+  writeBin(object=x, con=tmpSST)
+  nc <- nc_open(tmpSST)
+  
+  tempneg <- expand.grid(lon=ncvar_get(nc, varid = "longitude"),
+                         lat=ncvar_get(nc, varid = "latitude")) %>% 
+                         {bind_cols(.,data.frame(chl=as.vector(ncvar_get(nc,"chla"))))} #%>% 
+  #mutate(sst=ifelse(sst<(-2),-2,sst)) %>% 
+  #filter(!is.na(sst))
+  nc_close(nc)
+  
+  tempneg <- tempneg %>% 
+    mutate(lon=round(lon,2),
+           lat=round(lat,2)) %>% 
+    inner_join(statlkpVIIRS) %>% 
+    mutate(date=mydate$mydate[i])
+  
+  nbsdat <- nbsdat %>% 
+    bind_rows(tempneg)
+  
+  rm(tempneg)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data <- readRDS("ERDDAP_Chlorophyll/Data/EBS_Chlorophyll_Spring_VIIRS.RDS")
+
+data <- data %>% 
+  filter(!is.na(chl)) %>% 
+  inner_join(statlkpVIIRS)
+
+
+testebs <- nbsdat %>% 
+  left_join(mydate %>% rename(date=mydate))
+
+#  Summarize the chlorophyll data by month and year.
+testsumnbs <- data %>% 
+  inner_join(mydate %>% 
+               rename(date=mydate) %>% 
+               mutate(Year=as.numeric(year),
+                      Month=month.name[as.numeric(month)])) %>% 
+  group_by(Month,year) %>% 
+  summarise(chlvals=sum(!is.na(chl)),
+            chl=mean(chl,na.rm=TRUE)) %>% 
+  ungroup
+
+
+
+#----------------------------------------------------------------------
+#  Plot the data
+#----------------------------------------------------------------------
+
+# Blues (the fourth one is just white)
+OceansBlue1='#0093D0'
+WavesTeal3='#007078'
+SeagrassGreen1='#93D500'
+UrchinPurple1='#7F7FFF'
+CrustaceanOrange1='#FF8300'
+
+#png("Spring_Chlorophyll_EBS.png",width=10,height=7.5,units="in",res=300)
+testsum %>%
+  mutate(Month=fct_relevel(Month,"March","April","May")) %>% 
+  ggplot(aes(as.numeric(year),chl,color=Month)) + 
+  geom_line(size=1) + 
+  geom_point(size=2) + 
+  theme_bw() + 
+  xlab("Year") + 
+  ylab("Chlorophyll") + 
+  theme(legend.position=c(0.9,0.75),
+        axis.text=element_text(size=11),
+        legend.text = element_text(size=11),
+        legend.title = element_text(size=11)) + 
+  scale_x_continuous(breaks=c(1996:2019)) + 
+  scale_color_manual(values=c(OceansBlue1,SeagrassGreen1,WavesTeal3))
+dev.off()
+
+
+#png("Spring_Chlorophyll_NBS.png",width=10,height=7.5,units="in",res=300)
+testsumnbs %>%
+  mutate(Month=fct_relevel(Month,"March","April","May","June","July")) %>% 
+  ggplot(aes(as.numeric(year),chl,color=Month)) + 
+  geom_line(size=1) + 
+  geom_point(size=2) + 
+  theme_bw() + 
+  xlab("Year") + 
+  ylab("Chlorophyll") + 
+  theme(legend.position=c(0.9,0.75),
+        axis.text=element_text(size=11),
+        legend.text = element_text(size=11),
+        legend.title = element_text(size=11)) + 
+  scale_x_continuous(breaks=c(2006:2019)) + 
+  scale_color_manual(values=c(OceansBlue1,SeagrassGreen1,WavesTeal3,UrchinPurple1,CrustaceanOrange1))
+dev.off()
 
 
